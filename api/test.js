7fbf8852146f8bbe0285cfdf3d32fd1bc15b1a52
Travel Chatbot Developer <dev@travelchatbot.local>
2026-04-10 16:37:23 +0300

    Change to CommonJS syntax for Vercel compatibility

--- api/test.js
+++ api/test.js
@@ -1,4 +1,4 @@
-// Simple test function
-export default function handler(req, res) {
+// Simple test function using CommonJS
+module.exports = function handler(req, res) {
   res.status(200).json({ message: 'Hello from Vercel!', path: req.url, method: req.method });
 }
